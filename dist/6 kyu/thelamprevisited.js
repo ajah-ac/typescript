edd64f8fd6b0b2cfefa559d5760ca101742b0eab
ajah <ajahajang00@gmail.com>
2025-12-17 14:00:31 +0200

    Add to 6 kyu

---
+++ dist/6 kyu/thelamprevisited.js
@@ -0,0 +1,29 @@
+"use strict";
+/**Define a class called Lamp. It will have a string attribute for color and boolean attribute, on, that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign on as false on initialize.
+
+Give the lamp an instance method called toggleSwitch that will switch the value of the on attribute.
+
+Define another instance method called state that will return "The lamp is on." if it's on and "The lamp is off." otherwise.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Lamp = void 0;
+class Lamp {
+    constructor(color) {
+        this.color = color;
+        this.on = false;
+    }
+    toggleSwitch() {
+        if (this.on === false)
+            return this.on = true;
+        return this.on = false;
+    }
+    state() {
+        if (this.on === false) {
+            return `The lamp is off.`;
+        }
+        else {
+            return `The lamp is on.`;
+        }
+    }
+}
+exports.Lamp = Lamp;
